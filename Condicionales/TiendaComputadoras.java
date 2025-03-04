0fea5d175c9798a3bfe1e556637f5d3e9d9ba3d1
Carlos Mario Rojas Plaza <polid0192@gmail.com>
2025-03-03 22:01:40 -0500

    Add files via upload

---
+++ Condicionales/TiendaComputadoras.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class TiendaComputadoras {
+    public static void main(String[] args) {
+        
+         var precioPorComputadora = 500.0;
+        
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        
+        System.out.print("Ingrese el número de computadoras que desea adquirir: ");
+        int numeroComputadoras = scanner.nextInt();
+        
+        
+        var totalSinDescuento = numeroComputadoras * precioPorComputadora;
+        
+        
+        var descuento = 0.0;
+        
+        if (numeroComputadoras < 5) {
+            descuento = totalSinDescuento * 0.10; 
+        } else if (numeroComputadoras >= 5 & numeroComputadoras < 10) {
+            descuento = totalSinDescuento * 0.20; 
+        } else if (numeroComputadoras >= 10) {
+            descuento = totalSinDescuento * 0.40; 
+        }
+        
+        
+        var totalConDescuento = totalSinDescuento - descuento;
+        
+        System.out.println("\n---Total de compra de los computadores---");
+        System.out.println("\tComputadores seleccionados: " + numeroComputadoras);
+
+        System.out.println("\tTotal sin descuento: U$ " + totalSinDescuento);
+        System.out.println("\tDescuento aplicado: U$ " + descuento);
+        System.out.println("\tTotal con descuento: U$ " + totalConDescuento);
+        
+        
+        
+    }
+}
